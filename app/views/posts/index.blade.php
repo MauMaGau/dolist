9f1a135a459ec41b7a2938940825eb078601719d
David Townsend <dttownsend@gmail.com>
2014-06-26 12:44:54 +0100

    home page list

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-    <ul class="list-unstyled">
+    <ul class="nav nav-pills nav-stacked">
         @foreach($posts as $post)
-            <li class="clearfix">
-                <a href="{{ URL::route('posts.show', $post->id) }}">
+            <li>
+                <a href="{{ URL::route('posts.show', $post->id) }}" class="clearfix">
                     <h2 class="pull-left">{{ $post->title }}</h2>
                     <p><small class="pull-right">{{ date('l \t\h\e jS \o\f F\, Y', strtotime($post->created_at)) }}</small></p>
                 </a>
